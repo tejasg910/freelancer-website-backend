7e9f9cc605e5d7d79163be7c488601f4cfae0aec
tejasg910 <tejasgiri@ipangram.com>
2023-12-16 16:30:36 +0530

    fixed duplicate key error

--- db/index.js
+++ db/index.js
@@ -1,13 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(
-      process.env.MONGO_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      })
-}
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    autoIndex: false,
+  });
+};
 
-module.exports = { connectDB }
+module.exports = { connectDB };
--- models/Project.model.js
+++ models/Project.model.js
@@ -1,108 +1,119 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
+const projectSchema = new mongoose.Schema(
+  {
     projectTitle: {
-        type: String,
-//         required: true,
+      type: String,
+      //         required: true,
     },
     description: {
-        type: String,
-//         required: true,
+      type: String,
+      //         required: true,
     },
-    skills: [{
+    skills: [
+      {
         type: String,
-//         required: true,
-    }],
-    education: [{
+        //         required: true,
+      },
+    ],
+    education: [
+      {
         type: String,
-    }],
-    workLocation: [{
+      },
+    ],
+    workLocation: [
+      {
         type: String,
-        default: "remote"
-    }],
-    softwareRequirements: [{
-        type: String
-    }],
+        default: "remote",
+      },
+    ],
+    softwareRequirements: [
+      {
+        type: String,
+      },
+    ],
     freelancersCount: {
-        type: Number,
-//         required: true
+      type: Number,
+      //         required: true
     },
     duration: {
-        from: {
-            type: Date,
-            trim: true,
-            default: ''
-        },
-        to: {
-            type: Date,
-            trim: true,
-            default: ''
-        },
+      from: {
+        type: Date,
+        trim: true,
+        default: "",
+      },
+      to: {
+        type: Date,
+        trim: true,
+        default: "",
+      },
     },
-    visibility: [{
+    visibility: [
+      {
         type: String,
-    }],
+      },
+    ],
     budget: {
-        minPrice: Number,
-        maxPrice: Number,
-        currency: String
+      minPrice: Number,
+      maxPrice: Number,
+      currency: String,
     },
     postedBy: {
-        type: mongoose.Schema.ObjectId,
-        ref: "user"
+      type: mongoose.Schema.ObjectId,
+      ref: "user",
     },
     appliedBy: [
-        {
-            userId: {
-                type: mongoose.Schema.ObjectId,
-                ref: "user"
-            },
-            applicationId: {
-                type: mongoose.Schema.ObjectId,
-                ref: "application"
-            },
-        }
+      {
+        userId: {
+          type: mongoose.Schema.ObjectId,
+          ref: "user",
+        },
+        applicationId: {
+          type: mongoose.Schema.ObjectId,
+          ref: "application",
+        },
+      },
     ],
     favByUsers: [
-        {
-            type: mongoose.Schema.ObjectId,
-            ref: "user"      
-        }
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "user",
+      },
     ],
     hireRequests: [
-        {
-            freelancerId : {
-                type: mongoose.Schema.ObjectId,
-                ref: "user"
-            },
-            hireRequest : {
-                type: mongoose.Schema.ObjectId,
-                ref: "hireRequest"
-            },
-        }
-        
+      {
+        freelancerId: {
+          type: mongoose.Schema.ObjectId,
+          ref: "user",
+        },
+        hireRequest: {
+          type: mongoose.Schema.ObjectId,
+          ref: "hireRequest",
+        },
+      },
     ],
     hired: [
-        {
-            freelancerId : {
-                type: mongoose.Schema.ObjectId,
-                ref: "user", 
-                unique: true,
-            },
-            applicationId: {
-                type: mongoose.Schema.ObjectId,
-                ref: "application"
-            },
-            hireRequestId: {
-                type: mongoose.Schema.ObjectId,
-                ref: "hireRequest"
-            }
-        }
-    ]
-},
-{ timestamps: true })
+      {
+        freelancerId: {
+          type: mongoose.Schema.ObjectId,
+          ref: "user",
+          // unique: true,
+        },
+        applicationId: {
+          type: mongoose.Schema.ObjectId,
+          ref: "application",
+        },
+        hireRequestId: {
+          type: mongoose.Schema.ObjectId,
+          ref: "hireRequest",
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 // run this below line in mongo for full text search
 // db.projects.createIndex({ "$**" : "text" })
 
-module.exports = mongoose.model("project", projectSchema)
+module.exports = mongoose.model("project", projectSchema);
